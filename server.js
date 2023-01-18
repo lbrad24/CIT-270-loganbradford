4a7f513b8541787e757cca8f537c031cd578b2c7
Logan Bradford <lbradford2400@gmail.com>
2023-01-18 13:38:12 -0700

    updates to the server,js, package.json, and login.cmd files

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require ("express");
 
 const app = express();
 
-const port = 3000
+const port = 3000;
+
+const bodyParser = require ("body-parser");
+
+app.use(bodyParser.json()); //This looks for incoming data using body-parser
 
 app.get("/", (req, res) => {
     res.send("Hello Logan");
+});
+
+app.post('/login', (req, res) => {
+    const loginUser = req.body.userName;
+    console.log('Login username:'+loginUser);
+    res.send('Hello '+loginUser);
 });
 
 app.listen(port, () => {
