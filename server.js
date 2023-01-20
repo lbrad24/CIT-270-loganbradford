60a5be0dfba1bb9d8183847e0370cd5383b4bb1c
Logan Bradford <lbradford2400@gmail.com>
2023-01-20 13:47:57 -0700

    updated the app.post with if and else

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = 3000;
 
 const bodyParser = require ("body-parser");
 
+const {v4: uuidv4} = require('uuid'); //universally unique identifier
+
 app.use(bodyParser.json()); //This looks for incoming data using body-parser
 
 app.get("/", (req, res) => {
@@ -14,8 +16,16 @@ app.get("/", (req, res) => {
 
 app.post('/login', (req, res) => {
     const loginUser = req.body.userName;
+    const loginPassword = req.body.password; //Access the password data in the body
     console.log('Login username:'+loginUser);
-    res.send('Hello '+loginUser);
+    if (loginUser=="bra21039@byui.edu" && loginPassword=="P@55w0rd") {
+        const loginToken = uuidv4();
+        res.send(loginToken);
+    } else {
+        res.status(401);//unauthorized
+        res.send('Incorrect password for ' + loginUser);
+    }
+    
 });
 
 app.listen(port, () => {
